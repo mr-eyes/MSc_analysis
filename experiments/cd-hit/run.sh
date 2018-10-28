c25c6eb82234dbbce2524c7398910746cf27f2e6
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2018-10-28 04:11:03 +0200

    uncomment important line

--- experiments/cd-hit/run.sh
+++ experiments/cd-hit/run.sh
@@ -10,7 +10,7 @@ TYPE=${4}
 ASSESS=../../scripts/cdhit_assess_by_gene.py
 
 # Run CD-HIT
-#./${CDHIT} -c ${THRESHOLD} -T 0 -M 0 -n ${WORD} -d 0 -i $1 -o ${THRESHOLD}_${FASTA}
+./${CDHIT} -c ${THRESHOLD} -T 0 -M 0 -n ${WORD} -d 0 -i $1 -o ${THRESHOLD}_${FASTA}
 
 # Delete representative sequences file
 rm ${THRESHOLD}_${FASTA}
