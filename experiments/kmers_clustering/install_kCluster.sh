720dc49742a067e53494bc3cadad46c180d47438
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2018-11-01 22:03:09 +0200

    Install and run kCluster

---
+++ experiments/kmers_clustering/install_kCluster.sh
@@ -0,0 +1,9 @@
+# Clone and install kProcessor
+git clone https://github.com/dib-lab/Kprocessor --branch indexFusionGenes --single-branch kProcessor
+cd kProcessor
+make
+cd ..
+
+# Clone kCluster
+git clone https://github.com/mr-eyes/kCluster
+
--- experiments/kmers_clustering/run.sh
+++ experiments/kmers_clustering/run.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
 
-FILTER=../../scripts/filter_by_length.py
-CLUSTER=../../scripts/kallisto_kmer_clustering.py
-ASSESS=../../scripts/kallisto_assess_by_gene.py
-KALLISTO=kallisto/build/src/kallisto
+ASSESS=../../scripts/kCluster_assess_by_gene.py
+KPROCESSOR=kProcessor/Kprocessor
 FASTA=$(sed "s/.*\///" <<< $1)
 KMER=$2
 TYPE=$3
-# Filteration of any sequence with length more than Kmer-Size
-python $FILTER $1 filtered_K${KMER} ${KMER}
 
-# Generating kallisto index then GFA
-./${KALLISTO} index -i ${KMER}.idx filtered_K${KMER}_${FASTA} -k ${KMER}
-./${KALLISTO} inspect ${KMER}.idx --gfa=${KMER}.gfa
+# Generating kProcessor index
+echo "Generating index for kmer size: ${KMER}"
+#./${KPROCESSOR} index -i min_$FASTA --names min_$FASTA.names --method MAP -k ${KMER} -o ${TYPE}_k${KMER}_index
 
-# Delete Kallisto Index
-rm *idx
+# Generate kCluster Relations File
+#python kCluster/scripts/generate_relations.py ${TYPE}_k${KMER}_index.map ${TYPE}_${KMER}_index.namesMap min_$FASTA ${KMER} ${TYPE}_k${KMER}_relations
 
-# Perform Clustering of Kallisto's GFA Output
-python $CLUSTER ${KMER}.gfa clusters_k${KMER}.tsv
 
-# Delete the GFA file
-rm *gfa
+# Perform Clustering of kCluster Relations File (Thresholds 1:100 %)
+for THRESHOLD in {0..100..1};
+do pypy kCluster/scripts/relations_clustering.py ${TYPE}_k${KMER}_relations.tsv ${TYPE}_k${KMER}_index.namesMap $THRESHOLD;
+done
+
+# # Delete the original index file
+# rm ${TYPE}_${KMER}_index.map
 
 # Perform Clustering Assessment and save output files
-python $ASSESS filtered_K${KMER}* clusters_k${KMER}.tsv k${KMER}_clusters_assessment.tsv
+for THRESHOLD in {0..100..1};
+do pypy $ASSESS $1 clusters_c${THRESHOLD}.0_${TYPE}_k${KMER}_index.tsv kCluster_k${KMER}_${THRESHOLD}_assessment.tsv;
+done
 
-rm filtered_K${KMER}_${FASTA}
 
-mkdir -p ${TYPE}_results/K${KMER}_assessement
-mv *.tsv ${TYPE}_results/K${KMER}_assessement/
-mv *summary* ${TYPE}_results/K${KMER}_assessement/
-mv *stats.json ${TYPE}_results/K${KMER}_assessement/
+mkdir -p ${TYPE}/k${KMER}/{clusters,stats,summaries,details,visualization}
+mv *txt ${TYPE}/k${KMER}/summaries
+mv *json ${TYPE}/k${KMER}/stats
+mv *_assessment.tsv ${TYPE}/k${KMER}/details
+mv clusters*tsv ${TYPE}/k${KMER}/clusters
+rm *namesMap *relations.tsv min*
+
+# Visualize
+cd ${TYPE}/k${KMER}/summaries
+find *txt | sort -V | xargs grep "" | python ../../../../../scripts/visualize_kClusters_clustering.py kCluster_k${kmer}.html
+mv kCluster_k${kmer}.html ../visualization/
