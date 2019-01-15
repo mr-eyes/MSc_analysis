3fd1916d531f3766a10c80dd42f4a07439302bf0
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2019-01-15 22:28:36 +0200

    update clustering visualization to take summary directory as input

--- scripts/visualize_kClusters_clustering.py
+++ scripts/visualize_kClusters_clustering.py
@@ -1,74 +1,90 @@
+"""
+input : directory contains summary files |seqs\tclusters\ttype|
+summary*txt files must named with t{similarity_theshold} with trailing zeros to be sorted correctly
+"""
+
+
 import re
 import sys
 import os
 import plotly.plotly as py
 import plotly.graph_objs as go
 import plotly.offline
+from glob import glob
 
 output_html = "visualization.html"
+summary_dir = sys.argv[1]
 
-if len(sys.argv) > 1:
-    output_html = sys.argv[1]
-
-kmers = []
+thresholds = []
 types = {}
-
+counts = {}
 #kmers_count = os.popen('grep "" kmers_clustering/full_human_transcriptome_results/*/*summary*').read()
 
 #for line in kmers_count.split("\n"):
-for line in sys.stdin:
-    if len(line) < 5:
-        continue
 
-    _threshold = re.findall(r"_(\d+)", line)[0]
-    _value = re.findall(r":(\d+)", line)[0]
-    _type = re.findall(r"\[_(\w+)\]", line)[0]
-    _kmer_size = re.findall(r"k(\d+)", line)[0]
+for summary_file in sorted(glob(summary_dir + "/*sum*txt")):
+    f = open(summary_file, 'r')
+    next(f)  # skip headers
+    _threshold = re.findall(r"t(\d+)", summary_file)[0]
 
-    if "t_" + _threshold not in kmers:
-        kmers.append("t_" + _threshold)
-        
-    if _type not in types:
-        types[_type] = [_value]
-    else:
-        types[_type].append(_value)
+    thresholds.append(_threshold)
 
+    for line in f:
+        #print line
+        line = line.split()
+        seq_count = line[0]
+        clstrs_count = line[1]
+        _type = line[2]
+        #print _type
+
+        if _type not in types:
+            types[_type] = [clstrs_count]
+            counts[_type] = [seq_count]
+        else:
+            types[_type].append(clstrs_count)
+            counts[_type].append(seq_count)
+    f.close()
+
+print thresholds
+
+print types
+print counts
 
 complete_clean = go.Scatter(
-    x = kmers,
-    y = types["complete_clean"],
-    mode = 'lines+markers',
-    name = 'Complete Clean'
+    x=thresholds,
+    y=counts["cc"],
+    mode='lines+markers',
+    name='Complete Clean'
 )
 
 complete_mixed = go.Scatter(
-    x = kmers,
-    y = types["complete_mixed"],
-    mode = 'lines+markers',
-    name = 'Complete Mixed'
+    x=thresholds,
+    y=counts["cm"],
+    mode='lines+markers',
+    name='Complete Mixed'
 )
 
 incomplete_clean = go.Scatter(
-    x = kmers,
-     y = types["incomplete_clean"],
-    mode = 'lines+markers',
-    name = 'Incomplete Clean'
+    x=thresholds,
+    y=counts["ic"],
+    mode='lines+markers',
+    name='Incomplete Clean'
 )
 
 incomplete_mixed = go.Scatter(
-    x = kmers,
-    y = types["incomplete_mixed"],
-    mode = 'lines+markers',
-    name = 'Incomplete Mixed'
+    x=thresholds,
+    y=counts["im"],
+    mode='lines+markers',
+    name='Incomplete Mixed'
 )
 
 data = [complete_mixed, complete_clean, incomplete_mixed, incomplete_clean]
 
-layout = dict(title='<b>kClustering Biological Assessment | kSize = ' + _kmer_size + '</b>',
-              xaxis = dict(title = '<b>kCluster Thresholds</b>'),
-              yaxis = dict(title = '<b>Number Of Transcripts</b>', type="log"),
+layout = dict(title='<b>CD-HIT Clustering Assessment</b>',
+              xaxis=dict(title='<b>Threshold</b>', type='category'),
+              yaxis=dict(title='<b>Number Of Transcripts</b>', type="log"),
               )
 
 fig = dict(data=data, layout=layout)
 
-plotly.offline.plot(fig, filename = output_html, auto_open = True)
+plotly.offline.plot(fig, filename=output_html, auto_open=False)
